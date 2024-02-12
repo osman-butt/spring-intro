3b97e3fa029eafc35d87bbe9bc208f5c0e032d84
osman-butt <butt.osman@gmail.com>
2024-02-12 11:00:21 +0100

    PUT /persons/id

--- src/main/java/kea/exercise/person/controller/PersonController.java
+++ src/main/java/kea/exercise/person/controller/PersonController.java
@@ -2,10 +2,9 @@ package kea.exercise.person.controller;
 
 import kea.exercise.person.model.Person;
 import kea.exercise.person.repository.PersonRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -28,5 +27,28 @@ public class PersonController {
     public ResponseEntity<Person> getPerson(@PathVariable int id) {
         Optional<Person> person = personRepository.findById(id);
         return ResponseEntity.of(person);
+    }
+
+    @PostMapping("/persons")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Person createPerson(@RequestBody Person person) {
+        return personRepository.save(person);
+    }
+
+    @PutMapping("/persons/{id}")
+    public ResponseEntity<Person> updatePerson(@PathVariable int id, @RequestBody Person person) {
+        Optional<Person> original = personRepository.findById(id);
+        if (original.isPresent()) {
+            Person originalPerson = original.get();
+            // Update person
+            originalPerson.setFirstName(person.getFirstName());
+            originalPerson.setLastName(person.getLastName());
+            originalPerson.setDateOfBirth(person.getDateOfBirth());
+            // Save to DB and return the updated person
+            Person updatedPerson = personRepository.save(originalPerson);
+            return ResponseEntity.ok().body(updatedPerson);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 }
